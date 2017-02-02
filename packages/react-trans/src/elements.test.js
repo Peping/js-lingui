49a1197bc1cc6ec1bf0977e6d246f93930f85bd2
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2017-02-02 02:50:24 +0100

    fix: Fix name of imported package
    affects: react-trans

--- packages/react-trans/src/elements.test.js
+++ packages/react-trans/src/elements.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import { formatElements } from './format'
+import { formatElements } from './elements'
 
 
 describe('formatElements', function() {
